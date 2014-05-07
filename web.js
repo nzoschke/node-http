6a332ac84384756c65d112bed8cc78f307e46d6e
Noah Zoschke <noah@heroku.com>
2014-05-06 23:26:08 -0700

    web

---
+++ web.js
@@ -0,0 +1,10 @@
+var http = require("http")
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"})
+  response.end("Hello World\n")
+})
+
+var port = process.env.PORT || 5000
+server.listen(port)
+console.log("Server running on port " + port)
